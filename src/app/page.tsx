d84cb4ec24c9ced67dc47cb1ac92b05b2d0c84ad
Dennis Kimathi <kimathidennis38@gmail.com>
2025-06-16 15:57:00 +0300

    fix: fox ts issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,10 @@
-import Image from "next/image";
-import Map from "../components/Map";
+
 import CanopyCoverVisualizer from "@/components/CanopyCoverVisualizer";
 
 export default function Home() {
   return (
     <div className="">
       <main className="w-full h-full ">
-        {/* <Map 
-        polygonData={{
-          type: 'Polygon',
-          coordinates: [
-            [
-              [175.0872760438407, -41.15123827188798],
-              [175.0872760438407, -41.15148827188798],
-              [175.0875260438407, -41.15148827188798],
-              [175.0875260438407, -41.15123827188798],
-              [175.0872760438407, -41.15123827188798]
-            ]
-          ]
-        }}
-        /> */}
         <CanopyCoverVisualizer />
       </main>
     </div>
